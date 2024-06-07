8e479de8c474b15ddd6635b1aadc8f1ff1370e3e
svenN <veracelisc2@gmail.com>
2024-06-07 15:28:57 -0500

    Commit inicial

---
+++ src/app/verificar/verificar.page.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-verificar',
+  templateUrl: './verificar.page.html',
+  styleUrls: ['./verificar.page.scss'],
+})
+export class VerificarPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
